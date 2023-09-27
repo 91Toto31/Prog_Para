75dd5574b673dfa04d10280a388a8b7e6c01fb34
91Toto31 <146136703+91Toto31@users.noreply.github.com>
2023-09-27 19:15:45 +0200

    Update example7_Broadcast.c

--- example7_Broadcast.c
+++ example7_Broadcast.c
@@ -19,11 +19,8 @@ int main(int argc, char *argv[])
 	printf("Before Broadcast: I am the node %d and my message is %s\n", myid, message);
 	size = 10;
 
-	if(myid == 0)
-	 	for(i=1; i<npes;i++)
-	    	MPI_Send(message, size, MPI_CHAR, i, tag, MPI_COMM_WORLD);
-    else 
-		MPI_Recv(message, size, MPI_CHAR, 0, tag, MPI_COMM_WORLD, &status);
+	MPI_Send(message, size, MPI_CHAR, 1, 0, MPI_COMM_WORLD);
+    	MPI_Recv(message, size, MPI_CHAR, 1, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
 
 	MPI_Bcast(message, size, MPI_CHAR, 1, MPI_COMM_WORLD);
 
