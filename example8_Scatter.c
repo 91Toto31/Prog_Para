c48ff2e712ef7f9b84870ade7602e60fb952e655
91Toto31 <146136703+91Toto31@users.noreply.github.com>
2023-10-03 16:23:39 +0200

    Update example8_Scatter.c

--- example8_Scatter.c
+++ example8_Scatter.c
@@ -3,31 +3,34 @@
 #include <stdlib.h>
 #include <string.h>
 
-
 int main(int argc, char *argv[])
 {
-	int myid, npes, size,i;
-	char message[10];
-	int *vector;
-	int vector_rec[1];
+    int myid, npes;
+    float message[2]; // Changez le type de données en float et la taille du tableau en 2
+    float *vector;
+    float vector_rec[2]; // Changez la taille du tableau en 2
 
-	MPI_Init(&argc, &argv);
-	MPI_Comm_size(MPI_COMM_WORLD, &npes);	
-	MPI_Comm_rank(MPI_COMM_WORLD, &myid);
+    MPI_Init(&argc, &argv);
+    MPI_Comm_size(MPI_COMM_WORLD, &npes);
+    MPI_Comm_rank(MPI_COMM_WORLD, &myid);
 
-	if (myid == 0) {
-	   vector=(int *) malloc(npes*sizeof(int));
-	   for(i=0;i<npes;i++)
-		vector[i]=i*100+1;
-	}
-	else 
-	   vector=(int *) malloc(1*sizeof(int));
+    if (myid == 0) {
+        vector = (float *)malloc(2 * npes * sizeof(float)); // Modifiez la taille d'allocation
+        for (int i = 0; i < 2 * npes; i++) { // Modifiez la plage de la boucle
+            vector[i] = (float)i * 10.0 + 1.0; // Changez les valeurs de données
+        }
+    }
+    else {
+        vector = (float *)malloc(2 * sizeof(float)); // Modifiez la taille d'allocation
+    }
 
-	MPI_Scatter(vector,1,MPI_INT,vector_rec,1,MPI_INT,0,MPI_COMM_WORLD);
-	
-	printf("I am  %d vector_rec:%d \n", myid, vector_rec[0]);
+    MPI_Scatter(vector, 2, MPI_FLOAT, vector_rec, 2, MPI_FLOAT, 0, MPI_COMM_WORLD); // Changez le type de données et la taille
 
-	MPI_Finalize();
+    printf("Je suis %d, vector_rec : %.2f %.2f\n", myid, vector_rec[0], vector_rec[1]); // Changez le format d'affichage
+
+    free(vector); // Libérez la mémoire allouée dynamiquement
+    MPI_Finalize();
 }
+
 
 
